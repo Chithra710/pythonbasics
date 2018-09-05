f3830e447a71feeb14cc97875911cd5a9939adb5
Chithra710 <chithrasaba12@gmail.com>
2018-09-05 16:58:26 +0530

    Update strfind.py

--- strfind.py
+++ strfind.py
@@ -1,3 +1,3 @@
-
+#To find the substring in the string. if found, It returns the lowest index of the substring. IF not, it returns -1.
 decodede_response = 'what Did you know about suzy'
 decodede_response.find('Did you know')
